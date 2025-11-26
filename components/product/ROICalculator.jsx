beca830db5939291483926e16dde19eaea4c01a0
moizxox <164487066+moizxox@users.noreply.github.com>
2025-11-26 15:48:48 +0500

    layout static

--- components/product/ROICalculator.jsx
+++ components/product/ROICalculator.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ROICalculator = () => {
   return (
-    <h1>helo</h1>
+    
+    <section className='py-16 lg:py-20 px-5 border-b border-dashed border-gray-300'>
+      <section className='max-w-[1440px] mx-auto bg-red-500'>
+Hello World
+      </section>
+    </section>
   )
 }
 
