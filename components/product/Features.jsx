8c954faf9dec34e5e4bb115b198495807f0a83e3
moizxox <164487066+moizxox@users.noreply.github.com>
2025-11-27 11:38:12 +0500

    starter heading comp added

--- components/product/Features.jsx
+++ components/product/Features.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Label from '../ui/Label';
 import Feature1Image from '@/assets/images/feature-1.png';
 import Feature2Image from '@/assets/images/feature-2.png';
 import Feature3Image from '@/assets/images/feature-3.png';
 import Image from 'next/image';
+import Starter from '../ui/Starter';
 
 const Features = () => {
   const cards = [
@@ -27,16 +27,11 @@ const Features = () => {
   return (
     <section className="py-16 lg:py-20">
       <section className="mx-auto flex max-w-[1440px] flex-col gap-10 px-5">
-        <div className="mx-auto flex max-w-2xl flex-col items-center justify-center gap-5 text-center">
-          {' '}
-          <Label text="Features" />
-          <h3 className="text-2xl font-semibold md:text-3xl lg:text-5xl">
-            Powerful Features Built for Every Merchant{' '}
-          </h3>
-          <p className="text-xl">
-            From smart routing to fraud protection everything you need to scale securely.
-          </p>
-        </div>
+        <Starter
+          heading="Powerful Features Built for Every Merchant"
+          text="From smart routing to fraud protection everything you need to scale securely."
+          label="Features"
+        />
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {cards.map((card) => (
             <article
