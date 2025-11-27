ebff18782a19eb75dd90fcf93b53899f4d431b97
moizxox <164487066+moizxox@users.noreply.github.com>
2025-11-27 11:38:19 +0500

    roi calc added

--- components/product/ROICalculator.jsx
+++ components/product/ROICalculator.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Label from '../ui/Label';
+import Starter from '../ui/Starter';
+import Image from 'next/image';
+import ROIimage from '@/assets/images/roi-calc.svg';
 
 const ROICalculator = () => {
   return (
     <section className="border-b border-dashed border-gray-300 py-16 lg:py-20">
-      <section className="mx-auto max-w-[1440px] bg-red-200 px-5">
+      <section className="mx-auto flex max-w-[1440px] flex-col items-center gap-10 px-5">
+        <Starter
+          heading="See How Much You Could Save With Smarter Payments"
+          text="Our ROI calculator shows how much more you could make with lower fees, fewer declines, and faster payouts."
+          label="ROI Calculator"
+        />
+        <Image src={ROIimage} alt="ROI Calculator" />
       </section>
     </section>
   );
