31c247cc7b30960b7acba13431d76cbde2f57d7f
moizxox <164487066+moizxox@users.noreply.github.com>
2025-11-27 15:27:33 +0500

    Icon Slider fix

--- components/product/ImageCarousel.jsx
+++ components/product/ImageCarousel.jsx
@@ -1,21 +1,16 @@
 // components/ImageCarousel.jsx
 'use client';
-
 import Slider from 'react-slick';
 import Image from 'next/image';
-
-const images = [
-  '/home/section2/logo 1.png',
-  '/home/section2/logo 2.png',
-  '/home/section2/logo 3.png',
-  '/home/section2/logo 4.png',
-  '/home/section2/logo 5.png',
-  '/home/section2/logo 6.png',
-  '/home/section2/logo 7.png',
-  '/home/section2/logo 8.png',
-  //   '/home/section2/logo 9.png',
-  //   '/ihome/section2logo 10.png',
-];
+import Lgoo1 from '@/assets/images/logos/logo-1.png';
+import Lgoo2 from '@/assets/images/logos/logo-2.png';
+import Lgoo3 from '@/assets/images/logos/logo-3.png';
+import Lgoo4 from '@/assets/images/logos/logo-4.png';
+import Lgoo5 from '@/assets/images/logos/logo-5.png';
+import Lgoo6 from '@/assets/images/logos/logo-6.png';
+import Lgoo7 from '@/assets/images/logos/logo-7.png';
+import Lgoo8 from '@/assets/images/logos/logo-8.png';
+const images = [Lgoo1, Lgoo2, Lgoo3, Lgoo4, Lgoo5, Lgoo6, Lgoo7, Lgoo8];
 
 export default function ImageCarousel() {
   const settings = {
@@ -24,33 +19,27 @@ export default function ImageCarousel() {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1024, // tablet
-        settings: { slidesToShow: 3 },
+        settings: { slidesToShow: 4 },
       },
       {
         breakpoint: 768, // mobile
-        settings: { slidesToShow: 1 },
+        settings: { slidesToShow: 2 },
       },
     ],
   };
 
   return (
-    <section className="py-16 lg:py-20">
+    <section className="icon-slide py-16 lg:py-20">
       <section className="mx-auto max-w-[1440px] px-5">
         <Slider {...settings}>
           {images.map((src, index) => (
             <div key={index} className="p-2">
-              <Image
-                src={src}
-                alt={`Image ${index + 1}`}
-                width={500}
-                height={70}
-                className="w-full max-w-[150px] object-contain object-bottom"
-              />
+              <Image src={src} alt={`Image ${index + 1}`} className="w-full max-w-[150px]" />
             </div>
           ))}
         </Slider>
