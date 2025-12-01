83dfc698d8358fd3d3d1a565193d894869a5d224
moizxox <164487066+moizxox@users.noreply.github.com>
2025-12-01 11:14:30 +0500

    case studies page added

---
+++ app/(marketing)/case-studies/page.jsx
@@ -0,0 +1,12 @@
+import HeroStarter from '@/components/ui/HeroStarter'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <HeroStarter heading="Real Results From Real Businessess" text="Explore success stories that show the impact of better processing, smarter routing, and stable MIDs." label="All case studies"/>
+    </>
+  )
+}
+
+export default page
