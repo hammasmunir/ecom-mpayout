a2dc63a33f1833be56f576114bd0a6e11a281b5e
moizxox <164487066+moizxox@users.noreply.github.com>
2025-12-02 18:07:04 +0500

    omnx page added

--- components/case-studies/Benefits.jsx
+++ components/case-studies/Benefits.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Benefits = ({ text }) => {
+  const textArray = text.split(' || ');
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-2xl font-medium md:text-4xl">Additional Benefits</h2>
-      <p className="text-text">{text}</p>
+      {textArray.map((item, index) => (
+        <p key={index} className="text-text">
+          {item}
+        </p>
+      ))}
     </div>
   );
 };
