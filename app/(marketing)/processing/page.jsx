3c566743da4337ed5e979bd233245c51b591b3a6
moizxox <164487066+moizxox@users.noreply.github.com>
2025-12-03 14:38:38 +0500

    hero added to processing

---
+++ app/(marketing)/processing/page.jsx
@@ -0,0 +1,17 @@
+import HeroStarter from '@/components/ui/HeroStarter';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <HeroStarter
+        heading="The Smarter, Safer Way to Process Payments"
+        text="Discover how dedicated bank processing ends bans, freezes, and shared risk giving your business stability, control, and lasting freedom."
+        label="Processing"
+        maxWidth="800px"
+      />
+    </>
+  );
+};
+
+export default page;
--- components/ui/HeroStarter.jsx
+++ components/ui/HeroStarter.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import Label from './Label';
 
-const HeroStarter = ({
-  heading,
-  text,
-  noCenter,
-  label,
-  className,
-  noCustom = true,
-  maxWidth = '1440px',
-}) => {
+const HeroStarter = ({ heading, text, noCenter, label, className, noCustom, maxWidth }) => {
+  const width = maxWidth ? maxWidth : '1440px';
   return (
     <section className="relative w-full overflow-hidden">
       <div
-        className={`flex max-w-[1440px] flex-col ${!noCenter && 'mx-auto items-center justify-center text-center'} gap-4 ${!noCustom && 'relative z-10 mt-20 px-5 sm:mt-24 md:mt-28'} ${className}`}
+        className={`flex flex-col ${!noCenter && 'mx-auto items-center justify-center text-center'} gap-4 ${!noCustom && 'z-10 mt-20 px-5 pb-10 md:mt-28'} ${className}`}
+        style={{ maxWidth: width }}
       >
         <div
-          className={`flex max-w-[${maxWidth}] flex-col items-center gap-4 ${noCenter && 'items-start'}`}
+          className={`flex w-full flex-col items-center gap-4 ${noCenter && 'mx-auto items-start'}`}
         >
           <Label text={label} />
           <h1 className="mb-4 text-3xl font-semibold text-gray-900 sm:px-0 md:text-4xl lg:text-6xl">
