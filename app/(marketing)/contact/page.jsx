683214b83e4dee221183baa350c704cd53fbad01
moizxox <164487066+moizxox@users.noreply.github.com>
2025-12-03 11:42:35 +0500

    contact form added

--- app/(marketing)/contact/page.jsx
+++ app/(marketing)/contact/page.jsx
@@ -1,10 +1,15 @@
+'use client';
+import Dropdown from '@/components/ui/Dropdown';
 import HeroStarter from '@/components/ui/HeroStarter';
-import React from 'react';
+import React, { useState } from 'react';
 
-const page = () => {
+const Contact = () => {
+  const companyTypes = ['E-commerce', 'Retail', 'SaaS'];
+  const [selectedCompanyType, setSelectedCompanyType] = useState(null);
+
   return (
     <section className="border-border border-b-[3px] border-dashed py-16 lg:py-20">
-      <section className="mx-auto flex max-w-[1440px] flex-col items-center gap-20 px-5">
+      <section className="mx-auto grid max-w-[1440px] grid-cols-1 gap-20 px-5 lg:grid-cols-[1fr_0.8fr]">
         <HeroStarter
           heading="Get in Touch with Our Team"
           text="Talk to our experts for tailored guidance on payments, onboarding, and growth."
@@ -12,7 +17,7 @@ const page = () => {
           noCustom
           noCenter
         />
-        <form className="flex flex-col gap-4 rounded-xl bg-white p-7 shadow-[10px_-10px_75px_0px_#0357F040]">
+        <form className="flex w-full flex-col gap-4 rounded-xl bg-white p-7 shadow-[10px_-10px_75px_0px_#0357F040]">
           <div className="gap flex flex-col">
             <label htmlFor="firstName" className="text-text text-sm">
               First Name<span className="text-gray-500">*</span>
@@ -65,11 +70,20 @@ const page = () => {
             <label htmlFor="comapnyType" className="text-text text-sm">
               Company Type<span className="text-gray-500">*</span>
             </label>
+            <Dropdown
+              options={companyTypes}
+              value={selectedCompanyType}
+              onChange={setSelectedCompanyType}
+              className="bg-[#FAFAFA]"
+            />
           </div>
+          <button className="bg-primary-500 rounded-full px-4 py-2 text-white" type="submit">
+            Submit
+          </button>
         </form>
       </section>
     </section>
   );
 };
 
-export default page;
+export default Contact;
