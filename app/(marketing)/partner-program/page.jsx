10dc0756574f8d845f9f9b4353c716653d3e7199
moizxox <164487066+moizxox@users.noreply.github.com>
2025-12-03 12:19:57 +0500

    form program added on partner program

---
+++ app/(marketing)/partner-program/page.jsx
@@ -0,0 +1,75 @@
+'use client';
+import Form from '@/components/ui/Form';
+import HeroStarter from '@/components/ui/HeroStarter';
+import React from 'react';
+
+const PartnerProgram = () => {
+  const handleSubmit = (formData, e) => {
+    e.preventDefault();
+    console.log('Form submitted with data:', formData);
+  };
+  const formFields = [
+    {
+      name: 'firstName',
+      label: 'First Name',
+      type: 'text',
+      required: true,
+      bgColor: '#FAFAFA',
+    },
+    {
+      name: 'lastName',
+      label: 'Last Name',
+      type: 'text',
+      required: true,
+      bgColor: '#FAFAFA',
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      required: true,
+      bgColor: '#FAFAFA',
+    },
+    {
+      name: 'companyName',
+      label: 'Company Name',
+      type: 'text',
+      required: true,
+      bgColor: '#FAFAFA',
+    },
+    {
+      name: 'companyType',
+      label: 'Company Type',
+      type: 'dropdown',
+      options: ['E-commerce', 'Retail', 'SaaS'],
+      required: true,
+      bgColor: '#FAFAFA',
+    },
+  ];
+
+  return (
+    <section className="border-border border-b-[3px] border-dashed py-16 lg:py-20">
+      <section className="mx-auto grid max-w-[1440px] grid-cols-1 gap-10 px-5 lg:grid-cols-[1fr_0.8fr] lg:gap-20">
+        <HeroStarter
+          heading="Partner With Us, Grow Faster"
+          text="Earn recurring revenue while giving your clients a better way to process payments."
+          label="Partner Program"
+          noCustom
+          noCenter
+          maxWidth="500px"
+        />
+        <Form
+          fields={formFields}
+          onSubmit={handleSubmit}
+          formBgColor="bg-white"
+          submitButton={{
+            text: 'Submit',
+            className: 'bg-primary-500 rounded-full p-4 text-white cursor-pointer',
+          }}
+        />
+      </section>
+    </section>
+  );
+};
+
+export default PartnerProgram;
